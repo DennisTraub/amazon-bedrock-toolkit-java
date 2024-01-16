7f7d5f4f750d29c0a8f451919f31bf4b2307c95f
Dennis Traub <traubd@amazon.com>
2024-01-16 18:05:10 +0100

    Multiple scenarios

--- src/test/java/scenarios/ailabs/jurassic2/IT_Scenario_Jurassic_2.java
+++ src/test/java/scenarios/ailabs/jurassic2/IT_Scenario_Jurassic_2.java
@@ -4,6 +4,7 @@ import aws.community.toolkits.bedrock.SimpleBedrockClient;
 import aws.community.toolkits.bedrock.providers.ailabs.Jurassic2Request;
 import aws.community.toolkits.bedrock.providers.ailabs.Jurassic2Response;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
@@ -14,37 +15,55 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class IT_Scenario_Jurassic_2 {
 
+    private static SimpleBedrockClient client;
+
+    @BeforeAll
+    static void setup() {
+        client = new SimpleBedrockClient();
+    }
+
     @Test
-    void scenarios() {
-        SimpleBedrockClient client = new SimpleBedrockClient();
+    void simpleInvocation() {
         String prompt = "Hello";
 
-        // Simple invocation returns completion
         String simpleCompletion = client.invokeModel(AILABS_JURASSIC_2, prompt);
-        assertNotNull(simpleCompletion);
-        System.out.println(simpleCompletion);
 
-        // Using builder returns response object
+        assertNotNull(simpleCompletion);
+    }
+
+    @Test
+    void builderWithHighLevel() {
         Jurassic2Request request = Jurassic2Request.builder()
                 .prompt("Hello")
                 .temperature(0.5)
                 .build();
+
         Jurassic2Response response = client.invokeModel(request);
 
-        // Response object provides high-level API to get completion
         String highLevelCompletion = response.completion();
-        assertNotNull(highLevelCompletion);
-        System.out.println(highLevelCompletion);
 
-        // Response object provides low-level API from AWS SDK
+        assertNotNull(highLevelCompletion);
+    }
+
+    @Test
+    void builderWithLowLevel() {
+        Jurassic2Request request = Jurassic2Request.builder()
+                .prompt("Hello")
+                .temperature(0.5)
+                .build();
+
+        Jurassic2Response response = client.invokeModel(request);
+
         InvokeModelResponse sdkResponse = response.sdkResponse();
+
         JSONObject responseBody = new JSONObject(sdkResponse.body().asUtf8String());
+
         String lowLevelCompletion = responseBody
                 .getJSONArray("completions")
                 .getJSONObject(0)
                 .getJSONObject("data")
                 .getString("text");
+
         assertNotNull(lowLevelCompletion);
-        System.out.println(lowLevelCompletion);
     }
 }
